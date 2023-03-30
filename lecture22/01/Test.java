7b1c7d6ae6603bcd852978dd2fd4bb6aff27a58c
Mehrdad Sabetzadeh <mehrdad.sabetzadeh@uni.lu>
2023-03-29 21:25:42 -0400

    no message

--- lecture22/01/Test.java
+++ lecture22/01/Test.java
@@ -5,7 +5,7 @@ public class Test {
 		LinkedList<Integer> l = new LinkedList<Integer>();
 
 		for (int i=0; i<10; i++) {
-			l.add(new Integer(i));
+			l.add(i);
 		}
 
 		Iterator<Integer> theIterator = l.iterator();
@@ -15,7 +15,7 @@ public class Test {
 
 		int sizeOfTest = 10000;
 		for (int i=11; i<=sizeOfTest; i++) {
-			l.add(new Integer(i));
+			l.add(i);
 		}
 
 		Integer dummy;
